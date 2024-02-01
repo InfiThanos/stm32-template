2d2889cace47f6d85e7a518aed17b0b81b3d674e
InfiThanos <max-vasilevv@yandex.ru>
2024-02-01 17:24:27 +0300

    Add files for stm32f429zi

--- prod/src/main.c
+++ prod/src/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include "stm32f429xx.h"
 
 int main(void){
-    int a = 3;
+    /*int a = 3;
     int b = 4;
     char str[] = "Hello there!!!!!!!!!!";
     printf("%d %d %s", a, b, str);
+*/
+    RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+    GPIOB -> MODER = 0x10000000;
+    GPIOB -> OTYPER = 0; 
+    GPIOB -> OSPEEDR = 0; 
+    GPIOB -> ODR = 0x4000;
 
     while (1){}
     return 0;
